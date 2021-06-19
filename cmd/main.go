7840972f069ccb7f7c0b122b6eb116e64536a7be
鈴木泰斗 <suzukitaito@suzukitaitonoMacBook-Pro.local>
2021-06-20 00:51:41 +0900

    save

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,45 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GH_ACCESS_TOKEN")},
+	)
+	cli := github.NewClient(oauth2.NewClient(ctx, ts))
+	page := 1
+	for {
+		files, res, err := cli.PullRequests.ListFiles(
+			ctx,
+			"suzuito",
+			"blog1-data",
+			1,
+			&github.ListOptions{
+				Page: page,
+			},
+		)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%+v\n", err)
+			os.Exit(1)
+		}
+		if len(files) <= 0 {
+			break
+		}
+		for _, file := range files {
+			fmt.Println(*file.Filename, *file.Status)
+		}
+		fmt.Println(res)
+		if res.NextPage == res.LastPage {
+			break
+		}
+		page = res.NextPage
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/suzuito/blog1-data
 
 go 1.16
+
+require (
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
+	golang.org/x/oauth2 v0.0.0-20210615190721-d04028783cf1 // indirect
+)
